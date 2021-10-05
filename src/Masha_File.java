1570ef3b5954b77f7d8b878d9f246e2bf10f47ed
masha7v7p <Diana4342#>
2021-10-04 21:56:49 -0400

    Masha updated again  file

--- src/Masha_File.java
+++ src/Masha_File.java
@@ -1,5 +1,11 @@
 public class Masha_File {
     public static void main(String[] args) {
         System.out.println("This is Masha's file :)");
+
+        double a = 59;
+        double b = 56.0;
+        double sum = a * b;
+        double sumResult = sum;
+        System.out.println(sumResult);
     }
 }
